cf6eec786fe6eaf0a13e56ed9021fe6dda263c8c
Andrew Tram <MyMBP@Andrews-MacBook-Pro.local>
2017-03-16 11:07:59 -1000

    finished vault

--- vault.js
+++ vault.js
@@ -1,4 +1,22 @@
 'use strict';
 module.exports = function() {
-  
+
+  var vault = {};
+
+  function setValue(key, value){
+    vault[key] = value;
+  }
+
+  function getValue(key){
+    if(vault[key] === undefined){
+      return vault[key] = null;
+    } else {
+      return vault[key];
+    }
+  }
+
+  return {
+    setValue: setValue,
+    getValue: getValue
+  };
 };
